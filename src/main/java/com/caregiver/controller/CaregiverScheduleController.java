6b18966ef96885501b44fcaabfc129b0ec4d6e5a
I27312933-spec <l27312933@gmail.com>
2026-04-09 20:00:51 +0800

    补充实体类

--- src/main/java/com/caregiver/controller/CaregiverScheduleController.java
+++ src/main/java/com/caregiver/controller/CaregiverScheduleController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/caregiver-schedule")
+@RequestMapping("/api/caregiverSchedule")
 @CrossOrigin(origins = "*")
 public class CaregiverScheduleController {
 
